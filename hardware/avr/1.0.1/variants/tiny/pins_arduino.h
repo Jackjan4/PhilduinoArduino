dcf2f42637a9008e62174c04df564d47db2b0ef1
Unknown <Jackjan@protonmail.com>
2019-02-16 21:02:14 +0100

    Updated variants to Spence Konde's ATTinyCore

--- hardware/avr/1.0.1/variants/tiny/pins_arduino.h
+++ hardware/avr/1.0.1/variants/tiny/pins_arduino.h
@@ -3,6 +3,7 @@
   Part of Arduino - http://www.arduino.cc/
 
   Copyright (c) 2007 David A. Mellis
+  Copyright 2015~2018 Spence Konde
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -20,100 +21,212 @@
   Boston, MA  02111-1307  USA
 
   $Id: wiring.h 249 2007-02-03 16:52:51Z mellis $
-
-  Modified 28-08-2009 for attiny84 R.Wiersma
-  Modified 14-10-2009 for attiny45 Saposoft
 */
 
 #ifndef Pins_Arduino_h
 #define Pins_Arduino_h
 
+#define ATTINYX5 1       //backwards compatibility
+#define __AVR_ATtinyX5__ //this is recommended way
+#define USE_SOFTWARE_SPI 1
+
 #include <avr/pgmspace.h>
 
-#include "core_build_options.h"
+#define NUM_DIGITAL_PINS            6
+#define NUM_ANALOG_INPUTS           6
+#define analogInputToDigitalPin(p)  (((p) == 0) ? 5 : (((p) == 1) ? 2 : (((p) == 2) ? 4 :(((p) == 3) ? 3 : -1))))
 
-#if defined( __AVR_ATtinyX313__ )
-#define PORT_A_ID 1
-#define PORT_B_ID 2
-#define PORT_D_ID 4
+#define digitalPinHasPWM(p)         ((p) == 0 || (p) == 1 || (p)==4)
+
+#define ADC_TEMPERATURE 15
+
+//This part has a USI, not an SPI module. Accordingly, there is no MISO/MOSI in hardware. There's a DI and a DO. When the chip is used as master, DI is used as MISO, DO is MOSI; the defines here specify the pins for master mode, as SPI master is much more commonly used in Arduino-land than SPI slave, and these defines are required for compatibility. Be aware of this when using the USI SPI fucntionality (and also, be aware that the MISO and MOSI markings on the pinout diagram in the datasheet are for ISP programming, where the chip is a slave. The pinout diagram included with this core attempts to clarify this)
+
+#define SS   3
+#define MOSI 1
+#define MISO 0
+#define SCK  2
+
+#define USI_DDR_PORT DDRB
+#define USI_SCK_PORT DDRB
+#define USCK_DD_PIN DDB2
+#define DO_DD_PIN DDB1
+#define DI_DD_PIN DDB0
+
+#  define DDR_USI DDRB
+#  define PORT_USI PORTB
+#  define PIN_USI PINB
+#  define PORT_USI_SDA PORTB0
+#  define PORT_USI_SCL PORTB2
+#  define PIN_USI_SDA PINB0
+#  define PIN_USI_SCL PINB2
+#  define USI_START_VECTOR USI_START_vect
+#  define USI_OVERFLOW_VECTOR USI_OVF_vect
+#  define DDR_USI_CL DDR_USI
+#  define PORT_USI_CL PORT_USI
+#  define PIN_USI_CL PIN_USI
+#ifndef USI_START_COND_INT
+#  define USI_START_COND_INT USISIF
 #endif
 
-#if defined( __AVR_ATtinyX4__ )
-#define PORT_A_ID 1
-#define PORT_B_ID 2
-#endif
+static const uint8_t SDA = 0;
+static const uint8_t SCL = 2;
 
-#if defined( __AVR_ATtinyX5__ )
-#define PORT_B_ID 1
-#endif
+//Ax constants cannot be used for digitalRead/digitalWrite/analogWrite functions, only analogRead().
+static const uint8_t A0 = 0x80 | 0;
+static const uint8_t A1 = 0x80 | 1;
+static const uint8_t A2 = 0x80 | 2;
+static const uint8_t A3 = 0x80 | 3;
 
-#ifndef __AVR_ATtiny85__
-#define __AVR_ATtiny85__
-#endif
+#define PIN_B0  ( 0)
+#define PIN_B1  ( 1)
+#define PIN_B2  ( 2)
+#define PIN_B3  ( 3)
+#define PIN_B4  ( 4)
+#define PIN_B5  ( 5)
 
-#define NOT_A_PIN 0
-#define NOT_A_PORT 0
+#define LED_BUILTIN  (1)
 
-#define NOT_ON_TIMER 0
-#define TIMER0A 1
-#define TIMER0B 2
-#define TIMER1A 3
-#define TIMER1B 4
+//----------------------------------------------------------
+//----------------------------------------------------------
+//Core Configuration (used to be in core_build_options.h)
 
-//changed it to uint16_t to uint8_t
-extern const uint8_t PROGMEM port_to_mode_PGM[];
-extern const uint8_t PROGMEM port_to_input_PGM[];
-extern const uint8_t PROGMEM port_to_output_PGM[];
-extern const uint8_t PROGMEM port_to_pcmask_PGM[];
+//If Software Serial communications doesn't work, run the TinyTuner sketch provided with the core to give you a calibrated OSCCAL value.
+//Change the value here with the tuned value. By default this option uses the default value which the compiler will optimise out. 
+#define TUNED_OSCCAL_VALUE                        OSCCAL
+//e.g
+//#define TUNED_OSCCAL_VALUE                        0x57
 
-extern const uint8_t PROGMEM digital_pin_to_port_PGM[];
-// extern const uint8_t PROGMEM digital_pin_to_bit_PGM[];
-extern const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[];
-extern const uint8_t PROGMEM digital_pin_to_timer_PGM[];
 
-// Get the bit location within the hardware port of the given virtual pin.
-// This comes from the pins_*.c file for the active board configuration.
-// 
-// These perform slightly better as macros compared to inline functions
-//
-#define digitalPinToPort(P) ( pgm_read_byte( digital_pin_to_port_PGM + (P) ) )
-#define digitalPinToBitMask(P) ( pgm_read_byte( digital_pin_to_bit_mask_PGM + (P) ) )
-#define digitalPinToTimer(P) ( pgm_read_byte( digital_pin_to_timer_PGM + (P) ) )
-#define analogInPinToBit(P) (P)
-// in the following lines modified pgm_read_word in pgm_read_byte, word doesn't work on attiny45
-#define portOutputRegister(P) ( (volatile uint8_t *)( pgm_read_byte( port_to_output_PGM + (P))) )
-#define portInputRegister(P) ( (volatile uint8_t *)( pgm_read_byte( port_to_input_PGM + (P))) )
-#define portModeRegister(P) ( (volatile uint8_t *)( pgm_read_byte( port_to_mode_PGM + (P))) )
-#define portPcMaskRegister(P) ( (volatile uint8_t *)( pgm_read_byte( port_to_pcmask_PGM + (P))) )
+//Choosing not to initialise saves power and flash. 1 = initialise.
+#define INITIALIZE_ANALOG_TO_DIGITAL_CONVERTER    1
+#define INITIALIZE_SECONDARY_TIMERS               1
+/*
+  For various reasons, Timer 0 is a better choice for the millis timer on the
+  '85 processor.
+*/
+#define TIMER_TO_USE_FOR_MILLIS                   0
 
-#if defined(__AVR_ATtinyX5__)
+/*
+  Where to put the software serial? (Arduino Digital pin numbers)
+*/
+//WARNING, if using software, TX is on AIN0, RX is on AIN1. Comparator is favoured to use its interrupt for the RX pin.
+#define USE_SOFTWARE_SERIAL						  1
+//Please define the port on which the analog comparator is found.
+#define ANALOG_COMP_DDR						 	  DDRB
+#define ANALOG_COMP_PORT						  PORTB
+#define ANALOG_COMP_PIN						 	  PINB
+#define ANALOG_COMP_AIN0_BIT					  0
+#define ANALOG_COMP_AIN1_BIT					  1
+
+/*
+  Analog reference bit masks.
+*/
+// X 0 0 VCC used as Voltage Reference, disconnected from PB0 (AREF).
+#define DEFAULT (0)
+// X 0 1 External Voltage Reference at PB0 (AREF) pin, Internal Voltage Reference turned off.
+#define EXTERNAL (1)
+// 0 1 0 Internal 1.1V Voltage Reference.
+#define INTERNAL (2)
+#define INTERNAL1V1 INTERNAL
+// 1 1 1 Internal 2.56V Voltage Reference with external bypass capacitor at PB0 (AREF) pin(1).
+#define INTERNAL2V56 (7)
+// 1 1 0 Internal 2.56V Voltage Reference without external bypass capacitor, disconnected from PB0 (AREF)(1).
+#define INTERNAL2V56_NO_CAP (6)
+#define INTERNAL2V56NOBP INTERNAL2V56_NO_CAP
+
+#define digitalPinToInterrupt(p)  ((p) == 2 ? 0 : NOT_AN_INTERRUPT)
+
+//----------------------------------------------------------
+//----------------------------------------------------------
+//----------------------------------------------------------
+//----------------------------------------------------------
+
+
 #define digitalPinToPCICR(p)    (((p) >= 0 && (p) <= 5) ? (&GIMSK) : ((uint8_t *)NULL))
-#define digitalPinToPCICRbit(p) (PCIE)
+#define digitalPinToPCICRbit(p) 5
 #define digitalPinToPCMSK(p)    (((p) >= 0 && (p) <= 5) ? (&PCMSK) : ((uint8_t *)NULL))
 #define digitalPinToPCMSKbit(p) (p)
+
+#ifdef ARDUINO_MAIN
+
+// ATMEL ATTINY45 / ARDUINO
+//
+//                           +-\/-+
+//  Ain0       (D  5)  PB5  1|    |8   VCC
+//  Ain3       (D  3)  PB3  2|    |7   PB2  (D  2)  INT0  Ain1
+//  PWM  Ain2  (D  4)  PB4  3|    |6   PB1  (D  1)        PWM (OC0B)
+//  OC1B               GND  4|    |5   PB0  (D  0)        PWM (OC0A)
+//                           +----+
+
+// these arrays map port names (e.g. port B) to the
+// appropriate addresses for various functions (e.g. reading
+// and writing) tiny45 only port B 
+const uint16_t PROGMEM port_to_mode_PGM[] = 
+{
+	NOT_A_PORT,
+	NOT_A_PORT,
+	(uint16_t)&DDRB,
+};
+
+const uint16_t PROGMEM port_to_output_PGM[] = 
+{
+	NOT_A_PORT,
+	NOT_A_PORT,
+	(uint16_t)&PORTB,
+};
+
+const uint16_t PROGMEM port_to_input_PGM[] = 
+{
+	NOT_A_PIN,
+	NOT_A_PIN,
+	(uint16_t)&PINB,
+};
+
+const uint8_t PROGMEM digital_pin_to_port_PGM[] = 
+{
+	PB, /* 0 */
+	PB,
+	PB,
+	PB,
+	PB, 
+	PB, /* 5 */
+
+};
+
+const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = 
+{
+	_BV(0), /* 0, port B */
+	_BV(1),
+	_BV(2),
+	_BV(3), /* 3 port B */
+	_BV(4),
+	_BV(5),
+
+};
+
+const uint8_t PROGMEM digital_pin_to_timer_PGM[] = 
+{
+	TIMER0A, /* OC0A */
+	TIMER0B, /* OC0B */
+	NOT_ON_TIMER,
+	NOT_ON_TIMER, 
+	TIMER1B, /*OC1B*/
+	NOT_ON_TIMER,
+};
+
 #endif
 
-#if defined(__AVR_ATtinyX4__)
-#define digitalPinToPCICR(p)    (((p) >= 0 && (p) <= 10) ? (&GIMSK) : ((uint8_t *)NULL))
-#define digitalPinToPCICRbit(p) (((p) <= 2) ? PCIE1 : PCIE0)
-#define digitalPinToPCMSK(p)    (((p) <= 2) ? (&PCMSK1) : (((p) <= 10) ? (&PCMSK0) : ((uint8_t *)NULL)))
-#define digitalPinToPCMSKbit(p) (((p) <= 2) ? (p) : (10 - (p)))
 #endif
 
-#if defined(__AVR_ATtiny4313__)
-#define digitalPinToPCX(p,s1,s2,s3,s4,s5) \
-    (((p) >= 0) \
-        ? (((p) <=  1) ? (s1)  /*  0 -  1  ==>  D0 - D1 */  \
-        : (((p) <=  3) ? (s2)  /*  2 -  3  ==>  A1 - A0 */  \
-        : (((p) <=  8) ? (s3)  /*  4 -  8  ==>  D2 - D6 */  \
-        : (((p) <= 16) ? (s4)  /*  9 - 16  ==>  B0 - B7 */  \
-        : (s5))))) \
-        : (s5))
-//                                                   s1 D     s2 A     s3 D     s4 B
-#define digitalPinToPCICR(p)    digitalPinToPCX( p, &GIMSK,  &GIMSK,  &GIMSK,  &GIMSK,  NULL )
-#define digitalPinToPCICRbit(p) digitalPinToPCX( p, PCIE2,   PCIE1,   PCIE2,   PCIE0,   0    )
-#define digitalPinToPCMSK(p)    digitalPinToPCX( p, &PCMSK2, &PCMSK1, &PCMSK2, &PCMSK0, NULL )
-#define digitalPinToPCMSKbit(p) digitalPinToPCX( p, p,       3-p,     p-2,     p-9,     0    )
-#endif
 
-#endif
+
+
+//Old code, just here for temporary backup until I decide it is not needed.
+/*//WARNING, if using software, RX must be on a pin which has a Pin change interrupt <= 7 (e.g. PCINT6, or PCINT1, but not PCINT8)
+#define USE_SOFTWARE_SERIAL						  1
+//These are set to match Optiboot pins.
+#define SOFTWARE_SERIAL_PORT 					  PORTB
+#define SOFTWARE_SERIAL_TX 						  0
+#define SOFTWARE_SERIAL_PIN 					  PINB
+#define SOFTWARE_SERIAL_RX 						  1*/
